b2c661cbb91884f4505fdd473045531cb06bc83d
Woong <rlqmatk@naver.com>
2022-12-12 14:06:53 +0900

    Feat: 로그인 여부에 따른 Redirect 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './style/global-style';
 
 import TodoPage from './pages/Todo';
 import AuthPage from './pages/Auth';
 
 const App = () => {
+  const isLoggedIn = () => localStorage.getItem('token');
+
   return (
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<AuthPage />} />
-        <Route path="/todo" element={<TodoPage />} />
+        <Route
+          path="/"
+          element={
+            isLoggedIn() ? <Navigate to="/todo" replace /> : <AuthPage />
+          }
+        />
+        <Route
+          path="/todo"
+          element={isLoggedIn() ? <TodoPage /> : <Navigate to="/" replace />}
+        />
       </Routes>
     </>
   );
