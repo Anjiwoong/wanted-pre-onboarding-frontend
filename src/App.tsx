d29bcd9c89e31e54df740507fcd3f580ee392ecd
Woong <rlqmatk@naver.com>
2022-12-12 19:52:22 +0900

    주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from './style/global-style';
 
 import TodoPage from './pages/Todo';
 import AuthPage from './pages/Auth';
 
 const App = () => {
-  const isLoggedIn = () => localStorage.getItem('token');
-
   return (
     <>
       <GlobalStyle />
       <Routes>
-        {/* <Route
-          path="/"
-          element={
-            isLoggedIn() ? <Navigate to="/todo" replace /> : <AuthPage />
-          }
-        />
-        <Route
-          path="/todo"
-          element={
-            localStorage.getItem('token') ? (
-              <TodoPage />
-            ) : (
-              <Navigate to="/" replace />
-            )
-          }
-        /> */}
         <Route path="/" element={<AuthPage />} />
         <Route path="/todo" element={<TodoPage />} />
       </Routes>
